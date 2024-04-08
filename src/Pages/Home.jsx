7d1dad11880641c3a101eceee7c519afd507ad2a
KalpeshSuthar09 <sutharkalpesh101@gmail.com>
2024-04-08 23:28:07 +0530

    model imported

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,50 @@
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Loader from "../components/Loader";
+import { DirectionalLight } from "three";
+import Island from "../models/Island";
+import { Sky } from "@react-three/drei";
+
+const Home = () => {
+    const adjustIslandForScreenSize = () => {
+        let screenScale = null;
+        let screenPosition = [0, - 6.5, -43];
+        let rotation = [0.1, 4.7, 0];
+
+        if (window.innerWidth < 769) {
+            screenScale = [0.9, 0.9, 0.9];
+        } else {
+            screenScale = [1, 1, 1];
+        }
+        return [screenPosition, screenScale, rotation];
+    }
+
+    const [isLandScale, isLandPosition, isLandRotation] = adjustIslandForScreenSize();
+
+
+    return (
+        <section className="w-full h-screen relative">
+            {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+                POPUP
+            </div> */}
+
+            <Canvas className="w-full h-screen bg-transparent"
+                camera={{ near: 0.1, far: 1000 }}
+            >
+                <Suspense fallback={<Loader />} >
+                    <DirectionalLight position={[1, 1, 1]} intensity={2} />
+                    <ambientLight intensity={0.5} />
+                    <hemisphereLight skyColor="#b1e1ff" groundColor="000000" intensity={1} />
+                    <Sky />
+                    <Island
+                        position={isLandPosition}
+                        scale={isLandScale}
+                        rotate={isLandRotation}
+                    />
+                </Suspense>
+            </Canvas>
+        </section>
+    );
+};
+
+export default Home;
