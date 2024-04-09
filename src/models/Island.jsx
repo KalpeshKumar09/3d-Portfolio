ab4e33fdb7198e87b2ab487a01840802e2e5da49
KalpeshSuthar09 <sutharkalpesh101@gmail.com>
2024-04-09 15:21:08 +0530

    model complete

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -1,4 +1,14 @@
-/* eslint-disable react/no-unknown-property */
+/**
+ * IMPORTANT: Loading glTF models into a Three.js scene is a lot of work.
+ * Before we can configure or animate our model’s meshes, we need to iterate through
+ * each part of our model’s meshes and save them separately.
+ *
+ * But luckily there is an app that turns gltf or glb files into jsx components
+ * For this model, visit https://gltf.pmnd.rs/
+ * And get the code. And then add the rest of the things.
+ * YOU DON'T HAVE TO WRITE EVERYTHING FROM SCRATCH
+ */
+
 import { a } from "@react-spring/three";
 import { useEffect, useRef } from "react";
 import { useGLTF } from "@react-three/drei";
@@ -6,13 +16,13 @@ import { useFrame, useThree } from "@react-three/fiber";
 
 import islandScene from "../assets/3d/island.glb";
 
-const Island = ({
+export function Island({
     isRotating,
     setIsRotating,
     setCurrentStage,
     currentFocusPoint,
     ...props
-}) => {
+}) {
     const islandRef = useRef();
     // Get access to the Three.js renderer and viewport
     const { gl, viewport } = useThree();
@@ -235,5 +245,3 @@ const Island = ({
         </a.group>
     );
 }
-
-export default Island;
